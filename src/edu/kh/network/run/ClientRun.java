ae42c8ff2e16cf75a1de383c71eb1275a8a3ebc4
Jinho <thiszino@gmail.com>
2023-01-03 12:21:49 +0900

    client commit

---
+++ src/edu/kh/network/run/ClientRun.java
@@ -0,0 +1,14 @@
+package edu.kh.network.run;
+
+import edu.kh.network.model.service.TCPClient;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		
+		TCPClient client = new TCPClient();
+		client.clientStart();
+
+	}
+
+}
